40d8a7cb2668e70b7b4e9143e9e641d396b7448d
Bruno Barbosa <brunnobarbosas@gmail.com>
2022-03-21 22:54:50 -0300

    styles iniciados tela login

--- project-frontend-ilab-eqp3/src/App.jsx
+++ project-frontend-ilab-eqp3/src/App.jsx
@@ -1,44 +1,68 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Button from './components/Button'
+import {useState} from 'react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <main className="tela_login">
+      {/* <ToastContainer
+        className="toast-error"
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        theme="colored"
+      /> */}
+      <section className="tela_login_esquerda">
+        <h2 className="tela_login_esquerda_titulo">Gerencia todos os pagamentos da sua empresa em um só lugar.</h2>
+      </section>
+
+      <section className="tela_login_direita">
+        <h2 className="tela_login_direita_titulo">Faça seu login!</h2>
+
+        <form>
+          <div className="form_input email_login">
+            <label htmlFor="input-email" className="form_label_login">E-mail</label>
+            <input
+              className="input_login"
+              type="email"
+              name="input-email"
+              placeholder="Digite seu e-mail"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              required />
+          </div>
+
+          <div className="form_input senha_login botao">
+            <label htmlFor="input-senha" className="form_label_login">Senha</label>
+            <input
+              className="input_login"
+              type="password"
+              name="input-senha"
+              placeholder="Digite sua senha"
+              value={senha}
+              onChange={e => setEmail(senha.target.value)}
+              required
+            />
+          </div>
+
+          <div className="tela_login_direita_div_botao" >
+              <Button texto={"Entrar"} />
+          </div>
+
+        </form>
+      </section>
+    </main>
   )
 }
 
